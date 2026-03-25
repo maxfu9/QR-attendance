78ce8496db424baf0863c58b8254a8b35a722c5a
Kashif Bashir <kashif@Kashifs-MacBook-Air.local>
2026-03-25 23:28:19 +0500

    Fix scanner web route response handling

--- qr_attendance/www/scanner.py
+++ qr_attendance/www/scanner.py
@@ -1,18 +1,18 @@
 import frappe
 import os
 
+
 def get_context(context):
-	# Isolated view: bypass all theme/layout
 	app_path = frappe.get_app_path('qr_attendance')
-	# We'll use a version of scanner.html that is truly standalone
 	html_path = os.path.join(app_path, 'www', 'scanner.html')
 	if not os.path.exists(html_path):
 		html_path = os.path.join(app_path, 'qr_attendance', 'www', 'scanner.html')
-	
-	if not os.path.exists(html_path):
-		return "<h1>Error: scanner.html not found</h1>"
-		
-	html = frappe.read_file(html_path)
-	
-	from werkzeug.wrappers import Response
-	return Response(html, mimetype='text/html')
+
+	if os.path.exists(html_path):
+		frappe.local.response.type = 'text/html'
+		frappe.local.response.message = frappe.read_file(html_path)
+	else:
+		frappe.local.response.type = 'text/html'
+		frappe.local.response.message = '<h1>Error: scanner.html not found</h1>'
+
+	return context
